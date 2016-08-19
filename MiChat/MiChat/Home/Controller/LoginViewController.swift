67f423f4b3ae4ebda03e5ef3b91c05432e24365b
BBJMikey <605055291@qq.com>
2016-08-19 09:41:10 +0800

    提交Base

---
+++ MiChat/MiChat/Home/Controller/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  MiChat
+//
+//  Created by XFB on 16/8/19.
+//  Copyright © 2016年 mikey. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: BaseViewController {
+
+}
---
+++ MiChat/Tool/BaseClass/BaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.swift
+//  MiChat
+//
+//  Created by XFB on 16/8/19.
+//  Copyright © 2016年 mikey. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+}
